528cb01f21e2abdf6d1139cf597542b6b919c8ff
Abhishek <abhishek.tripathi456@gmail.com>
2019-04-13 23:16:01 +0530

    gcnfg

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 
-
+import datetime
 # NCLT url
 # field_bench_target_id = "5378"
 # title = "300"
@@ -26,17 +26,28 @@ nclt_url = make_nclt_url("5378", "300", "2018")
 # yr = "2019"
 # bench = "5378"
 # sub_bench = "5394"
+def toDateStr(x): return x.strftime('%d, %m, %Y').split(',')
+
+
+dr = [
+    datetime.date.fromordinal(ordinal)
+    for ordinal in range(
+        start_date.toordinal(),
+        end_date.toordinal(),
+    )
+]
+
 
 def make_nclt_StatusCauseurl(bench, sub_bench, year):
     yr = year
     bench = bench
     sub_bench = sub_bench
-    nclt_StatusCauseList = "https://nclt.gov.in/status-of-cause-list?field_date_value%5Bvalue%5D%5Bday%5D=4&field_date_value%5Bvalue%5D%5Bmonth%5D=4&field_date_value%5Bvalue%5D%5Byear%5D=" + \
-        yr + "&field_bench_target_id=" + bench + \
-        "&field_bench_court_target_id_entityreference_filter=" + sub_bench
+    [d, m, yr] = toDateStr(dr[0])
+
+    nclt_StatusCauseList = "https://nclt.gov.in/status-of-cause-list?field_date_value%5Bvalue%5D%5Bday%5D=" + d + "&field_date_value%5Bvalue%5D%5Bmonth%5D=" + \
+        m + "&field_date_value%5Bvalue%5D%5Byear%5D=" + yr + "&field_bench_target_id=" + \
+            bench + "&field_bench_court_target_id_entityreference_filter=" + sub_bench
     return nclt_StatusCauseList
 
 
 nclt_StatusCauseList = make_nclt_StatusCauseurl("5378", "5394", "2019")
-
-
